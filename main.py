fcb808e7b52c723f37c82a4b6d6fd9109e6615f8
Mordecai Mengesteab <mordecai.g.men@gmail.com>
2025-09-13 15:12:46 -0400

    made openai compatible whisper api

---
+++ main.py
@@ -0,0 +1,27 @@
+from openai import OpenAI
+import subprocess
+
+# kill server and restart it
+subprocess.run(["pkill", "-f", "uvicorn transcription:app"])
+subprocess.Popen(
+    ["uvicorn", "transcription:app", "--host", "0.0.0.0", "--port", "8000"]
+)
+
+# point to your local server
+client = OpenAI(
+    base_url="http://localhost:8000/v1",
+    api_key="fake-key",  # openai client requires this even for local
+)
+
+client.models.list()
+
+audio_file = open("audio1.wav", "rb")
+transcript = client.audio.transcriptions.create(
+    file=audio_file,
+    model="whisper-1",
+    response_format="verbose_json",
+    timestamp_granularities=["word", "segment"],
+)
+
+print(transcript.words)
+print(transcript.segments)
